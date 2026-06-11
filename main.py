3633d3f574197b149ab783ee0e94e32f2e146848
Jayakrishna k <145297170+Jaya2255@users.noreply.github.com>
2026-06-11 15:22:00 +0530

    Add TURN server configuration to RTC settings

--- main.py
+++ main.py
@@ -205,8 +205,16 @@ def main():
             rtc_configuration={
         "iceServers": [
             {"urls": ["stun:stun.l.google.com:19302"]},
-            {"urls": ["stun:stun1.l.google.com:19302"]},
-            {"urls": ["stun:stun2.l.google.com:19302"]}
+             {
+                    "urls": [
+                        "turn:openrelay.metered.ca:80",
+                        "turn:openrelay.metered.ca:443",
+                        "turn:openrelay.metered.ca:443?transport=tcp",
+                        "turns:openrelay.metered.ca:443?transport=tcp"
+                    ],
+                    "username": "openrelayproject",
+                    "credential": "openrelayproject"
+                }
         ]
     },
             media_stream_constraints={
